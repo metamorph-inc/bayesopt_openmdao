0946ce49f04742e4954292526e3832307c0708d5
Jonathon Williams <jwilliams@metamorphsoftware.com>
2017-02-28 16:20:33 -0600

    Initial skeleton project

---
+++ examples/optimizer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from __future__ import division
+
+import bayesopt_openmdao
+
+def main():
+    print("Bayesopt OpenMDAO Optimizer example")
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="bayesopt_openmdao",
+    version="0.1",
+    packages=find_packages(exclude=["examples"]),
+
+    install_requires=["openmdao>=1.7.1"]
+)
