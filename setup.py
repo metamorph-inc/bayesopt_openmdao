153edefdb2f2c36c674230b2ab6a723a3aec6647
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-03-21 11:53:21 -0500

    Note GitHub URL

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name="bayesopt_openmdao",
     version="0.1",
     packages=find_packages(exclude=["examples"]),
+    url='https://github.com/metamorph-inc/bayesopt_openmdao',
 
     install_requires=["openmdao>=1.7.1", "bayesopt"]
 )
